1fefbd06edbd6b54c87a7a4655d1b0d164c9f699
mmarks13 <michael.marks@berkeley.edu>
2016-02-17 16:53:57 -0800

    major overhaul with absolute directories

--- load_data_lake.sh
+++ load_data_lake.sh
@@ -3,36 +3,27 @@ if (( $EUID = 0 )); then
     su - w205
 fi
 
-#make sure we are in the home directory
-cd
-
-#download the file
-wget https://data.medicare.gov/views/bg9k-emty/files/Nqcy71p9Ss2RSBWDmP77H1DQXcyacr2khotGbDHHW_s?content_ type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip
-
-#rename the file in its existing directory. 
-mv Nqcy71p9Ss2RSBWDmP77H1DQXcyacr2khotGbDHHW_s\?content_type\=application%2Fzip\;\ charset\=binary Hospital_Data
+#download the file save a Hospital_Data in home directory
+wget https://data.medicare.gov/views/bg9k-emty/files/Nqcy71p9Ss2RSBWDmP77H1DQXcyacr2khotGbDHHW_s?content_ type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip -O /home/w205/Hospital_Data
 
 #Create a folder for all the unzipped files
-mkdir Ex1_Raw_Hospital_Data
+mkdir /home/w205/Ex1_Raw_Hospital_Data
 
 #move the zip file there
-mv Hospital_Data Ex1_Raw_Hospital_Data
-
-#change directories
-cd Ex1_Raw_Hospital_Data/
+mv /home/w205/Hospital_Data /home/w205/Ex1_Raw_Hospital_Data
 
 #unzip the file
-unzip Hospital_Data 
+unzip /home/w205/Ex1_Raw_Hospital_Data/Hospital_Data 
 
 #remove the spaces in the filenames
 for file in *.csv; do mv $file ${file//[[:space:]]}; done
 
 #rename our tables of interest
-mv HospitalGeneralInformation.csv hospitals.csv
-mv TimelyandEffectiveCare-Hospital.csv effective_care.csv
-mv ReadmissionsandDeaths-Hospital.csv readmissions.csv
-mv hvbp_hcahps_05_28_2015.csv survey_responses.csv
-mv MeasureDates.csv measure_dates.csv
+mv /home/w205/Ex1_Raw_Hospital_Data/HospitalGeneralInformation.csv /home/w205/Ex1_Raw_Hospital_Data/hospitals.csv
+mv /home/w205/Ex1_Raw_Hospital_Data/TimelyandEffectiveCare-Hospital.csv /home/w205/Ex1_Raw_Hospital_Data/effective_care.csv
+mv /home/w205/Ex1_Raw_Hospital_Data/ReadmissionsandDeaths-Hospital.csv /home/w205/Ex1_Raw_Hospital_Data/readmissions.csv
+mv /home/w205/Ex1_Raw_Hospital_Data/hvbp_hcahps_05_28_2015.csv /home/w205/Ex1_Raw_Hospital_Data/survey_responses.csv
+mv /home/w205/Ex1_Raw_Hospital_Data/MeasureDates.csv /home/w205/Ex1_Raw_Hospital_Data/measure_dates.csv
 
 #make HDFS directory for this exercise
 hdfs dfs -mkdir /user/w205/hospital_compare
@@ -43,8 +34,8 @@ hdfs dfs -mkdir /user/w205/hospital_compare/tbl_survey_responses
 hdfs dfs -mkdir /user/w205/hospital_compare/tbl_measure_dates
 
 #Move our tables of interest into HDFS
-hdfs dfs -put Ex1_Raw_Hospital_Data/hospitals.csv /user/w205/hospital_compare/tbl_hospitals/hospitals
-hdfs dfs -put Ex1_Raw_Hospital_Data/effective_care.csv /user/w205/hospital_compare/tbl_effective_care/effective_care
-hdfs dfs -put Ex1_Raw_Hospital_Data/readmissions.csv /user/w205/hospital_compare/tbl_readmissions/readmissions
-hdfs dfs -put Ex1_Raw_Hospital_Data/survey_responses.csv /user/w205/hospital_compare/tbl_survey_responses/survey_responses
-hdfs dfs -put Ex1_Raw_Hospital_Data/measure_dates.csv /user/w205/hospital_compare/tbl_measure_dates/measure_dates
+hdfs dfs -put /home/w205/Ex1_Raw_Hospital_Data/Ex1_Raw_Hospital_Data/hospitals.csv /user/w205/hospital_compare/tbl_hospitals/hospitals
+hdfs dfs -put /home/w205/Ex1_Raw_Hospital_Data/Ex1_Raw_Hospital_Data/effective_care.csv /user/w205/hospital_compare/tbl_effective_care/effective_care
+hdfs dfs -put /home/w205/Ex1_Raw_Hospital_Data/Ex1_Raw_Hospital_Data/readmissions.csv /user/w205/hospital_compare/tbl_readmissions/readmissions
+hdfs dfs -put /home/w205/Ex1_Raw_Hospital_Data/Ex1_Raw_Hospital_Data/survey_responses.csv /user/w205/hospital_compare/tbl_survey_responses/survey_responses
+hdfs dfs -put /home/w205/Ex1_Raw_Hospital_Data/Ex1_Raw_Hospital_Data/measure_dates.csv /user/w205/hospital_compare/tbl_measure_dates/measure_dates
