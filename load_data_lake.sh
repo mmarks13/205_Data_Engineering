36c60cde795eb3d556838c794d01b6b048e7b2ed
mmarks13 <michael.marks@berkeley.edu>
2016-02-17 16:28:12 -0800

    removed absolute path

--- load_data_lake.sh
+++ load_data_lake.sh
@@ -5,7 +5,7 @@ su - w205
 cd
 
 #delete existing directory that this code creates. 
-rm -rf /home/w205/Ex1_Raw_Hospital_Data/
+rm -rf Ex1_Raw_Hospital_Data/
 
 #download the file
 wget https://data.medicare.gov/views/bg9k-emty/files/Nqcy71p9Ss2RSBWDmP77H1DQXcyacr2khotGbDHHW_s?content_ type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip
