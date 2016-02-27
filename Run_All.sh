853ed1e2d1eefa47144a9b6357c0fa24aea62c4b
mmarks13 <michael.marks@berkeley.edu>
2016-02-26 19:04:32 -0800

    update

--- Run_All.sh
+++ Run_All.sh
@@ -26,8 +26,12 @@ hive -f /home/w205/Ex_1_github/hive_base_ddl.sql
 /data/spark15/bin/spark-submit /home/w205/Ex_1_github/investigations/hospital_variability/hospital_variability.py
 /data/spark15/bin/spark-submit /home/w205/Ex_1_github/investigations/hospitals_and_patients/hospitals_and_patients.py
 
+echo "Top Ten Hospitals for Chosen Quality Measures"
 hdfs dfs -cat /user/w205/hospital_compare_INVESTIGATIONS/best_hospitals/*
+
+echo "Top Ten States for Chosen Quality Measures"
 hdfs dfs -cat /user/w205/hospital_compare_INVESTIGATIONS/best_states/*
+
+echo "Top Ten Variable Measures Between Hospitals"
 hdfs dfs -cat /user/w205/hospital_compare_INVESTIGATIONS/hospital_variability/*
-hdfs dfs -cat /user/w205/hospital_compare_INVESTIGATIONS/hospitals_and_patients/*
 
